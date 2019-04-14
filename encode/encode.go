a3e0d1668a2638e798243a6653baf2c10eacd39d
pscn <pschnebel@gmail.com>
2019-04-14 21:15:30 +0200

    Remove ModTime from encoded file

--- encode/encode.go
+++ encode/encode.go
@@ -6,9 +6,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"path"
-	"time"
 )
 
 // File GZIPs the file given by filename as string, embeing the base of
@@ -19,22 +17,16 @@ func File(filename string) (*[]byte, error) {
 		return nil, fmt.Errorf("failed to read file: filename=%s; error=%s",
 			filename, err)
 	}
-	stat, err := os.Stat(filename)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get stat from file: filename=%s; error=%s",
-			filename, err)
-	}
-	return Bytes(&data, path.Base(filename), stat.ModTime())
+	return Bytes(&data, path.Base(filename))
 }
 
 // Bytes GZIPs the []bytes and returns a BASE64 encoded string, embeding
 // and name
-func Bytes(data *[]byte, name string, modTime time.Time) (*[]byte, error) {
+func Bytes(data *[]byte, name string) (*[]byte, error) {
 	var buf bytes.Buffer
 	zw := gzip.NewWriter(&buf)
 	// only Latin in GZIP headers
 	zw.Name = base64.StdEncoding.EncodeToString([]byte(name))
-	zw.ModTime = modTime
 	_, err := zw.Write(*data)
 	if err != nil {
 		return nil, fmt.Errorf("failed to compress data: name=%s; error=%s",
--- encode/encode_test.go
+++ encode/encode_test.go
@@ -2,29 +2,21 @@ package encode
 
 import (
 	"testing"
-	"time"
 )
 
 func TestEncode(t *testing.T) {
 	for _, m := range []struct {
 		content []byte
 		name    string
-		date    string
 		encoded string
 	}{
 		{
 			[]byte("Hello World\n"),
 			"hello_world.txt",
-			"2019-04-12T04:24:00Z",
-			`H4sICOASsFwA/2FHVnNiRzlmZDI5eWJHUXVkSGgwAPJIzcnJVwjPL8pJ4QIEAAD//+PllbAMAAAA`,
+			`H4sICAAAAAAA/2FHVnNiRzlmZDI5eWJHUXVkSGgwAPJIzcnJVwjPL8pJ4QIEAAD//+PllbAMAAAA`,
 		},
 	} {
-		// Mon Jan 2 15:04:05 -0700 MST 2006
-		now, err := time.Parse(time.RFC3339, m.date)
-		if err != nil {
-			t.Fatalf("test setup failed to parse date: %s; error=%s", m.date, err)
-		}
-		encoded, err := Bytes(&m.content, m.name, now)
+		encoded, err := Bytes(&m.content, m.name)
 		if err != nil {
 			t.Fatalf("failed to encode bytes: %s", err)
 		}
